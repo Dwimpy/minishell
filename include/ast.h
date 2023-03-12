c4cf5e9d5e40e619982c651247b035d2370232c9
arobu <arobu@student.42heilbronn.de>
2023-03-12 22:03:22 +0100

    small additions

---
+++ include/ast.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ast.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arobu <arobu@student.42heilbronn.de>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/12 17:01:50 by arobu             #+#    #+#             */
+/*   Updated: 2023/03/12 20:35:38 by arobu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef AST_H
+# define AST_H
+
+typedef struct s_ast_node	t_ast_node;
+
+typedef enum e_node_type
+{
+	COMMAND_NODE,
+	
+}				t_node_type;
+
+typedef struct s_ast_node
+{
+	
+}				t_ast_node;
+
+#endif
