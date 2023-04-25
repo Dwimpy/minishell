33f4dcf777e4f6156b495f9beb48723585e23872
Timo Killinger <tkilling@2-E-2.42heilbronn.de>
2023-04-25 12:33:35 +0200

    x_redirect good now

--- src/x_redirect.c
+++ src/x_redirect.c
@@ -6,7 +6,7 @@
 /*   By: tkilling <tkilling@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 13:17:07 by tkilling          #+#    #+#             */
-/*   Updated: 2023/04/24 13:34:16 by tkilling         ###   ########.fr       */
+/*   Updated: 2023/04/25 12:31:10 by tkilling         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,32 @@
 #include "sys/wait.h"
 #include "sys/stat.h"
 
-int	ft_re_stdout(int *stdout_cp, int fd)
+int	ft_re_stdout(int *stdout_cp, int *fd)
 {
-	if (fd < 0)
+	if (*fd < 0)
 		return (1);
-	*stdout_cp = dup(STDOUT_FILENO);
-	close(STDOUT_FILENO);
-	dup2(fd, STDOUT_FILENO);
-	close(fd);
+	else
+	{
+		*stdout_cp = dup(STDOUT_FILENO);
+		close(STDOUT_FILENO);
+		dup2(*fd, STDOUT_FILENO);
+		close(*fd);
+	}
 	return (0);
 }
 
-int	ft_re_stdin(int *stdin_cp, int fd)
+int	ft_re_stdin(int *stdin_cp, int *fd, t_ast_node *root)
 {
-	if (fd < 0)
+	*fd = open(root->data.command.input.filename, O_RDONLY);
+	if (*fd < 0)
 		return (1);
-	*stdin_cp = dup(STDIN_FILENO);
-	close(STDIN_FILENO);
-	dup2(fd, STDIN_FILENO);
-	close(fd);
+	else
+	{
+		*stdin_cp = dup(STDIN_FILENO);
+		close(STDIN_FILENO);
+		dup2(*fd, STDIN_FILENO);
+		close(*fd);
+	}
 	return (0);
 }
 
@@ -42,27 +49,20 @@ int	ft_redirect(t_ast_node *root, int *stdin_cp, int *stdout_cp)
 {
 	int		fd;
 
-	if (root->data.command.output.filename
-		&& !root->data.command.output.is_appended)
+	if (root->data.command.output.filename && !root->data.command.output.is_appended)
 	{
 		fd = open(root->data.command.output.filename, O_WRONLY | O_TRUNC, 0644);
-		if (ft_re_stdout(stdout_cp, fd) == 1)
+		if (ft_re_stdout(stdout_cp, &fd) == 1)
 			return (1);
 	}
-	else if (root->data.command.output.filename
-		&& root->data.command.output.is_appended)
+	else if (root->data.command.output.filename && root->data.command.output.is_appended)
 	{
-		fd = open(root->data.command.output.filename,
-				O_WRONLY | O_APPEND, 0644);
-		if (ft_re_stdout(stdout_cp, fd) == 1)
+		fd = open(root->data.command.output.filename, O_WRONLY | O_APPEND, 0644);
+		if (ft_re_stdout(stdout_cp, &fd) == 1)
 			return (1);
 	}
 	if (root->data.command.input.filename)
-	{
-		fd = open(root->data.command.input.filename, O_RDONLY);
-		if (ft_re_stdout(stdin_cp, fd) == 1)
-			return (1);
-	}
+		return (ft_re_stdin(stdin_cp, &fd, root));
 	return (0);
 }
 
